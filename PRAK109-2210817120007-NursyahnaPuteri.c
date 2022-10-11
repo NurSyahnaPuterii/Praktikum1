50243782ff5fc9f3f7eb8ffbb7847bd825efa9d9
Nursyahna Puteri <114853207+NurSyahnaPuterii@users.noreply.github.com>
2022-10-11 12:44:11 +0800

    Add files via upload

---
+++ PRAK109-2210817120007-NursyahnaPuteri.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main () {
+    
+    int pasukan_Yu_Zhong = 958730;
+    int pasukan_Cardia_Riverlands = 5;
+
+    printf (" Jumlah pasukan yang dibawa Yu zhong %d\n", pasukan_Yu_Zhong);
+    printf (" Jumlah pahlawan %d\n", pasukan_Cardia_Riverlands);
+    printf (" Jumlah pasukan yang harus dikalahkan setiap pahlawan adalah %d\n", pasukan_Yu_Zhong / pasukan_Cardia_Riverlands);
+}
