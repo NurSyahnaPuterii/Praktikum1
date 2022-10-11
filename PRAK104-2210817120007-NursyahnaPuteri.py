e09b424ec5e3219f7f236d64705d9db61121dd61
Nursyahna Puteri <114853207+NurSyahnaPuterii@users.noreply.github.com>
2022-10-11 12:40:37 +0800

    Add files via upload

---
+++ PRAK104-2210817120007-NursyahnaPuteri.py
@@ -0,0 +1,7 @@
+A = 400000
+B = 350000
+
+print ("Harga sepatu A adalah", A)
+print( "Harga sepatu B adalah", B)
+print ("Sepatu A mendapat diskon 13% sehingga bayarnya menjadi ", A - (A*13/100) )
+print ("Sepatu B mendapat diskon 21% sehingga harganya menjadi ", B - (B*21/100) )
