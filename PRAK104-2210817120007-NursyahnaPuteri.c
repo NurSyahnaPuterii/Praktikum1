191a29ceabed98dd7741c2f3cdd6637f38f72bed
Nursyahna Puteri <114853207+NurSyahnaPuterii@users.noreply.github.com>
2022-10-11 12:40:19 +0800

    Add files via upload

---
+++ PRAK104-2210817120007-NursyahnaPuteri.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main () {
+
+   int A = 400000;
+   int B = 350000;
+   
+   printf ("Harga sepatu A adalah %d\n", A);
+   printf ("Harga sepatu B adalah %d\n", B);
+   printf ("Sepatu A mendapat diskon 13%% sehingga bayarnya menjadi %d\n", A - (A*13/100) );
+   printf ("Sepatu B mendapat diskon 21%% sehingga harganya menjadi %d\n", B - (B*21/100) );
+}
