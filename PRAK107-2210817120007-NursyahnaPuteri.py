4aac7bf2e8202e0a7bd6b55f400bbf776fa7fd4b
Nursyahna Puteri <114853207+NurSyahnaPuterii@users.noreply.github.com>
2022-10-11 12:43:00 +0800

    Add files via upload

---
+++ PRAK107-2210817120007-NursyahnaPuteri.py
@@ -0,0 +1,12 @@
+a = 4
+b = 5
+c = 7
+hargapermeter= 85000
+keliling = a + b + c
+
+print ("Diketahui :")
+print ("Panjang sisi segitiga berturut-turut adalah", a, b , c)
+print ("Keliling Tanah Pak Dengklek adalah", keliling)
+print ("Harga tanah Per Meter adalah", hargapermeter)
+print ("Jawaban :")
+print ("Biaya yang diperlukan Pak Dengklek adalah Rp", keliling * hargapermeter)
