ac13f05bb766571b5bb4d18cacd275f9ba0c090b
Nursyahna Puteri <114853207+NurSyahnaPuterii@users.noreply.github.com>
2022-10-11 12:44:34 +0800

    Add files via upload

---
+++ PRAK109-2210817120007-NursyahnaPuteri.py
@@ -0,0 +1,6 @@
+pasukan_Yu_Zhong = 958730
+pasukan_Cardia_Riverlands = 5
+
+print (" Jumlah pasukan yang dibawa Yu zhong", pasukan_Yu_Zhong)
+print (" Jumlah pahlawan", pasukan_Cardia_Riverlands)
+print (" Jumlah pasukan yang harus dikalahkan setiap pahlawan adalah", round(pasukan_Yu_Zhong / pasukan_Cardia_Riverlands,))
